7ec93c2fe32904a560618f43a1690dd6da4e54e1
SamuelRuizVargas <samuel.ruizv@udea.edu.co>
2021-04-19 20:22:35 -0500

    Diseño de los circuito e intento de lograr encender todos los leds

--- parcial_11.ino
+++ parcial_11.ino
@@ -1,12 +1,33 @@
+int j = 0;
+
+int k;
+
 void setup()
 {
+  Serial.begin(9600);
+  // 74HC595
+  pinMode(3, OUTPUT); 
+  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT);
+  
   pinMode(13, OUTPUT);
+}
+
+void prueba()
+{
+  for(k = 0; k<100; k++)
+  {
+    for(int i=0; i<8; i++)
+    {
+      digitalWrite(store, LOW);
+      digitalWrite(store, HIGH);
+      j++;
+    }
+    j = 0;
+  }
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  prueba();
 }

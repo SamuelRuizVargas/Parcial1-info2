df89a46821fe913d5716eaebe36278f1eabed4da
SamuelRuizVargas <samuel.ruizv@udea.edu.co>
2021-04-20 17:20:23 -0500

    Conexiones terminadas exitosamente, codigo para encendido total de los leds

--- parcial_11.ino
+++ parcial_11.ino
@@ -1,27 +1,43 @@
-int j = 0;
+const int datos = 2; //Informacion (Entrada)
+const int salida = 3; //Reloj de salida
+const int despla = 4; //Reloj de desplazamiento
 
+// contador de columnas
+int j = 0;
+// tiempo en pantalla
 int k;
+
+//byts del inicio de cada fila
+int fila[8] = {127, 191, 223, 239, 247, 251, 253, 254};
+
+int LetraR[8] = {0, 60, 34, 34, 60, 40, 36, 34};
+
+int TodoEncendido[8] = {255, 255, 255, 255, 255, 255, 255, 255};
+
 
 void setup()
 {
   Serial.begin(9600);
   // 74HC595
-  pinMode(3, OUTPUT); 
-  pinMode(4, OUTPUT);
-  pinMode(2, OUTPUT);
+  pinMode(datos, OUTPUT); // Entrada de datos
+  pinMode(salida, OUTPUT); // Salida de datos
+  pinMode(despla, OUTPUT); // Desplazamiento
   
   pinMode(13, OUTPUT);
 }
 
-void prueba()
+void Iluminar()
 {
   for(k = 0; k<100; k++)
   {
     for(int i=0; i<8; i++)
     {
-      digitalWrite(store, LOW);
-      digitalWrite(store, HIGH);
+      digitalWrite(salida, LOW);
+      shiftOut(datos, despla, LSBFIRST, TodoEncendido[j]);
+      shiftOut(datos, despla, LSBFIRST, fila[i]);
+      digitalWrite(salida, HIGH);
       j++;
+      //delay(500);
     }
     j = 0;
   }
@@ -29,5 +45,5 @@ void prueba()
 
 void loop()
 {
-  prueba();
+  Iluminar();
 }
